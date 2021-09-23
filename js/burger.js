c97df483afad284ec540260ef9b5b3a88e458ba7
Deni Peace <65755052+Eto-Dedog@users.noreply.github.com>
2021-09-23 12:19:02 +0300

    Add files via upload

--- js/burger.js
+++ js/burger.js
@@ -1,14 +1,11 @@
-const burger = document.querySelector('.hamburger-menu')
+const burger = document.querySelector('.humburger-menu')
 const menuElem = document.querySelector('.menu')
 
 const toggleMenu = () => {
     menuElem.classList.toggle('menu-active')
-    burger.classList.toggle('hamburger-menu-active')
-
+    burger.classList.toggle('humburger-menu-active')
 }
 
-burger.addEventListener('click', () => toggleMenu())
-
-
-// humburger-menu
-// memu
+burger.addEventListener('click', () => {
+    toggleMenu()
+})
